6f1c173ef0d0b9c2a5bbf0e26fccf654fe6715bf
ornab74 <242115042+ornab74@users.noreply.github.com>
2026-07-28 18:40:24 -0400

    Export runtime v2 APIs

--- python/pufferforge/__init__.py
+++ python/pufferforge/__init__.py
@@ -2,8 +2,13 @@
 
 from .config import TrainConfig
 from .envs import NativeLineWorld, PythonVectorEnv, StepBatch
-from .models import ActorCritic
+from .models import ActorCritic, RecurrentActorCritic
+from .registry import make, register, registered, spec
+from .replay import PrioritizedReplay, ReplaySample
+from .selfplay import EloLeague, Player
+from .spaces import Box, Discrete, MultiDiscrete
 from .trainer import PPOTrainer, TrainMetrics
+from .wrappers import ClipReward, FrameStack, NormalizeObservation
 
 try:
     from . import _core
@@ -13,14 +18,11 @@ except ImportError:
     NATIVE_AVAILABLE = False
 
 __all__ = [
-    "ActorCritic",
-    "NATIVE_AVAILABLE",
-    "NativeLineWorld",
-    "PPOTrainer",
-    "PythonVectorEnv",
-    "StepBatch",
-    "TrainConfig",
-    "TrainMetrics",
+    "ActorCritic", "Box", "ClipReward", "Discrete", "EloLeague", "FrameStack",
+    "MultiDiscrete", "NATIVE_AVAILABLE", "NativeLineWorld", "NormalizeObservation",
+    "PPOTrainer", "Player", "PrioritizedReplay", "PythonVectorEnv",
+    "RecurrentActorCritic", "ReplaySample", "StepBatch", "TrainConfig",
+    "TrainMetrics", "make", "register", "registered", "spec",
 ]
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
